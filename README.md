08947d861b3568aa6c85908933b8f13bdab8df8a
osorosia <64416398+osorosia@users.noreply.github.com>
2022-12-19 06:31:01 +0900

    equal

--- README.md
+++ README.md
@@ -5,7 +5,9 @@
 - [ ] map
 
 - [x] std::iterator_traits
-- [ ] std::is_itegral
-- [ ] std::equal and/or std::lexicographical_compale
+- [ ] std::enable_if
+- [ ] std::is_integral
+- [x] std::equal 
+- [ ] (and/or) std::lexicographical_compale
 - [ ] std::pair
 - [ ] std::make_pair
---
+++ src/equal.hpp
@@ -0,0 +1,28 @@
+#ifndef EQUAL_HPP
+#define EQUAL_HPP
+
+namespace ft {
+
+template < class InputIt1, class InputIt2 >
+bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2) {
+    for (; first1 != last1; ++first1, ++first2) {
+        if (!(*first1 == *first2)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+template < class InputIt1, class InputIt2, class BinaryPredicate >
+bool equal(InputIt1 first1, InputIt1 last1, InputIt2 first2, BinaryPredicate p) {
+    for (; first1 != last1; ++first1, ++first2) {
+        if (!p(*first1, *first2)) {
+            return false;
+        }
+    }
+    return true;
+}
+
+} // namespace ft
+
+#endif /* EQUAL_HPP */
