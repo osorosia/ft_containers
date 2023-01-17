bba12e644735892d703fbb29f703bb389096ed10
osorosia <64416398+osorosia@users.noreply.github.com>
2023-01-17 19:50:43 +0900

    tiny fix

--- src/pair_test.cpp
+++ src/pair_test.cpp
@@ -23,26 +23,26 @@ void pair_operator_eq_test() {
 }
 
 void pair_operator_comp_test() {
-    ft::pair< int, int >  x0(0, 0);
-    std::pair< int, int > y0(0, 0);
+    ft::pair< int, int >  ft0(0, 0);
+    std::pair< int, int > std0(0, 0);
 
     for (int i = -1; i <= 1; i++) {
         for (int j = -1; j <= 1; j++) {
-            ft::pair< int, int >  x1(i, j);
-            std::pair< int, int > y1(i, j);
+            ft::pair< int, int >  ft1(i, j);
+            std::pair< int, int > std1(i, j);
 
-            assert((x0 == x1) == (y0 == y1));
-            assert((x0 != x1) == (y0 != y1));
-            assert((x0 < x1) == (y0 < y1));
-            assert((x0 > x1) == (y0 > y1));
-            assert((x0 <= x1) == (y0 <= y1));
-            assert((x0 >= x1) == (y0 >= y1));
+            assert((ft0 == ft1) == (std0 == std1));
+            assert((ft0 != ft1) == (std0 != std1));
+            assert((ft0 < ft1) == (std0 < std1));
+            assert((ft0 <= ft1) == (std0 <= std1));
+            assert((ft0 > ft1) == (std0 > std1));
+            assert((ft0 >= ft1) == (std0 >= std1));
         }
     }
 }
 
 void make_pair_test() {
-    ft::pair<int, int> x = ft::make_pair(1, 2);
+    ft::pair< int, int > x = ft::make_pair(1, 2);
     assert(x.first == 1);
     assert(x.second == 2);
 }
