790945c6fe83674ef8817e8da23557fbe7712946
osorosia <64416398+osorosia@users.noreply.github.com>
2023-01-17 19:51:19 +0900

    wip

--- src/vector_test.cpp
+++ src/vector_test.cpp
@@ -2,9 +2,22 @@
 
 void vector_empty_test() {
     TODO();
-    ft::vector<int> vec;
+    ft::vector< int > vec;
     assert(vec.empty());
 }
+
+void vector_size_test() {
+    TODO();
+    ft::vector< int > vec;
+    assert(vec.size() == 0);
+}
+
+void vector_op_eq_test() { TODO(); }
+void vector_op_ne_test() { TODO(); }
+void vector_op_lt_test() { TODO(); }
+void vector_op_le_test() { TODO(); }
+void vector_op_gt_test() { TODO(); }
+void vector_op_ge_test() { TODO(); }
 
 void vector_test() {
     TEST_NAME("vector");
@@ -17,11 +30,18 @@ void vector_test() {
     // Iterator
     // Capacity
     vector_empty_test();
+    vector_size_test();
     // Modifiers
 
     //
     // Non member functions
     //
+    vector_op_eq_test();
+    vector_op_ne_test();
+    vector_op_lt_test();
+    vector_op_le_test();
+    vector_op_gt_test();
+    vector_op_ge_test();
 
     OK();
 }
