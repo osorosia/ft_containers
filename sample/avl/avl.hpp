1ec4539af68b2222e244f9f68236eb7c75d7aeaf
osorosia <64416398+osorosia@users.noreply.github.com>
2023-02-20 04:11:11 +0900

    avl

--- sample/avl/avl.hpp
+++ sample/avl/avl.hpp
@@ -5,60 +5,13 @@
 #include <cstddef>
 
 struct Node {
-    Node* parent_;
     Node* left_;
     Node* right_;
+    Node* parent_;
     long  val_;
 
-    Node(long val) : parent_(NULL), left_(NULL), right_(NULL), val_(val) {}
-
-    void insertNode(long num) {
-        if (val_ == num)
-            return;
-        else if (num < val_) {
-            if (left_ == NULL) {
-                left_          = new Node(num);
-                left_->parent_ = this;
-            } else {
-                left_->insertNode(num);
-            }
-        } else {
-            if (right_ == NULL) {
-                right_          = new Node(num);
-                right_->parent_ = this;
-            } else {
-                right_->insertNode(num);
-            }
-        }
-    }
-
-    Node* eraseNode(long num) {
-        if (num == val_) {
-            if (!left_)
-                return right_;
-            else if (!right_)
-                return left_;
-            else {
-                Node* tmpRight = right_;
-                Node* tmpLeft  = left_;
-
-                Node* node     = right_;
-                Node* prevNode = this;
-                while (node->left_) {
-                    prevNode = node;
-                    node     = node->left_;
-                }
-                prevNode->left_ = NULL;
-                return node;
-            }
-        } else {
-            if (num < val_)
-                left_ = left_->eraseNode(num);
-            else
-                right_ = left_->eraseNode(num);
-            return this;
-        }
-    }
+    Node() : left_(NULL), right_(NULL), parent_(NULL), val_(0) {}
+    Node(long value) : left_(NULL), right_(NULL), parent_(NULL), val_(value) {}
 };
 
 struct AVLTree {
@@ -66,18 +19,30 @@ struct AVLTree {
 
     AVLTree() : root_(NULL) {}
 
-    void insert(long num) {
-        if (!root_) {
-            root_ = new Node(num);
-            return;
+    void insert(long value) {
+        if (root_ == NULL) {
+            root_ = new Node(value);
+        } else {
+            insertNode(root_, value);
         }
-
-        root_->insertNode(num);
     }
-    void erase(long num) {
-        if (!root_)
-            return;
-        root_ = root_->eraseNode(num);
+
+    void insertNode(Node* node, long value) {
+        if (node->val_ > value) {
+            if (node->left_) {
+                insertNode(node->left_, value);
+            } else {
+                node->left_          = new Node(value);
+                node->left_->parent_ = node;
+            }
+        } else if (node->val_ < value) {
+            if (node->right_) {
+                insertNode(node->right_, value);
+            } else {
+                node->right_          = new Node(value);
+                node->right_->parent_ = node;
+            }
+        }
     }
 };
 
