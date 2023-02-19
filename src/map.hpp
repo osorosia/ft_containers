fe8e3f6590c39a7b275ed1429e1389bd39f68274
osorosia <64416398+osorosia@users.noreply.github.com>
2023-02-20 03:50:50 +0900

    map

--- src/map.hpp
+++ src/map.hpp
@@ -5,9 +5,34 @@
 #include <memory>
 
 namespace ft {
+
+// https://en.cppreference.com/w/cpp/container/map
 template < class Key, class T, class Compare = std::less< Key >,
            class Allocator = std::allocator< std::pair< const Key, T > > >
-class map {};
+class map {
+protected:
+    //
+    // Member types
+    //
+    typedef Key                       key_type;
+    typedef T                         mapped_type;
+    typedef std::pair< const Key, T > value_type;
+    typedef std::size_t               size_type;
+    typedef std::ptrdiff_t            difference_type;
+    typedef Compare                   key_compare;
+    typedef Allocator                 allocator_type;
+    typedef value_type&               reference;
+    typedef const value_type&         const_reference;
+    typedef Allocator::pointer        pointer;
+    typedef Allocator::const_pointer  const_pointer;
+    // iterator
+    // const_iterator
+    // typedef std::reverse_iterator< iterator >       reverse_iterator;
+    // typedef std::reverse_iterator< const_iterator > const_reverse_iterator;
+
+    
+};
+
 } // namespace ft
 
 #endif /* MAP_HPP */
