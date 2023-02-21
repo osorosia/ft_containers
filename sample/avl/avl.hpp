a89b76b2def4365cb29d2a8c2192d9813dada415
osorosia <64416398+osorosia@users.noreply.github.com>
2023-02-22 01:29:39 +0900

    avl

--- sample/avl/avl.hpp
+++ sample/avl/avl.hpp
@@ -3,6 +3,8 @@
 
 #include <cassert>
 #include <cstddef>
+#include <iostream>
+using namespace std;
 
 struct Node {
     Node* left_;
@@ -10,14 +12,34 @@ struct Node {
     Node* parent_;
     long  val_;
 
-    Node() : left_(NULL), right_(NULL), parent_(NULL), val_(0) {}
-    Node(long val) : left_(NULL), right_(NULL), parent_(NULL), val_(val) {}
+    Node()
+        : left_(NULL)
+        , right_(NULL)
+        , parent_(NULL)
+        , val_(0) {}
+    Node(long val)
+        : left_(NULL)
+        , right_(NULL)
+        , parent_(NULL)
+        , val_(val) {}
+
+    Node* findMin() {
+        if (left_ == NULL)
+            return this;
+        return left_->findMin();
+    }
+    Node* findMax() {
+        if (right_ == NULL)
+            return this;
+        return right_->findMax();
+    }
 };
 
 struct AVLTree {
     Node* root_;
 
-    AVLTree() : root_(NULL) {}
+    AVLTree()
+        : root_(NULL) {}
 
     void insert(long val) {
         if (root_ == NULL) {
@@ -41,6 +63,86 @@ struct AVLTree {
             } else {
                 node->right_          = new Node(val);
                 node->right_->parent_ = node;
+            }
+        }
+    }
+
+    void erase(long val) { eraseNode(root_, val); }
+
+    void eraseNode(Node* node, long val) {
+        if (node == NULL) {
+            return;
+        }
+
+        if (node->val_ > val) {
+            eraseNode(node->left_, val);
+        } else if (node->val_ < val) {
+            eraseNode(node->right_, val);
+        } else if (node->val_ == val) {
+            if (node->left_ == NULL && node->right_ == NULL) {
+                // none
+                if (node->parent_) {
+                    if (node->parent_->left_ == node) {
+                        node->parent_->left_ = NULL;
+                    } else {
+                        node->parent_->right_ = NULL;
+                    }
+                } else {
+                    root_ = NULL;
+                }
+                delete node;
+            } else if (node->left_ == NULL) {
+                // a right child
+                if (node->parent_) {
+                    if (node->parent_->left_ == node) {
+                        node->parent_->left_ = node->right_;
+                    } else {
+                        node->parent_->right_ = node->right_;
+                    }
+                    node->right_->parent_ = node->parent_;
+                } else {
+                    root_ = node->right_;
+                }
+                delete node;
+            } else if (node->right_ == NULL) {
+                // a left child
+                if (node->parent_) {
+                    if (node->parent_->left_ == node) {
+                        node->parent_->left_ = node->left_;
+                    } else {
+                        node->parent_->right_ = node->left_;
+                    }
+                    node->left_->parent_ = node->parent_;
+                } else {
+                    root_ = node->left_;
+                    node->left_->parent_ = NULL;
+                }
+                delete node;
+            } else {
+                // both children
+                Node* tmp = node->right_->findMin();
+                if (tmp->parent_->left_ == tmp) {
+                    tmp->parent_->left_ = NULL;
+                } else {
+                    tmp->parent_->right_ = NULL;
+                }
+
+                tmp->left_   = node->left_;
+                tmp->right_  = node->right_;
+                tmp->parent_ = node->parent_;
+
+                tmp->left_->parent_  = tmp;
+                tmp->right_->parent_ = tmp;
+                if (node->parent_) {
+                    if (node->parent_->left_ == node) {
+                        node->parent_->left_ = tmp;
+                    } else {
+                        node->parent_->right_ = tmp;
+                    }
+                } else {
+                    root_ = tmp;
+                }
+                delete node;
             }
         }
     }
