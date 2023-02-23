36523dc74732894ecd9796915328a062cbcea3b4
osorosia <64416398+osorosia@users.noreply.github.com>
2023-02-24 00:14:55 +0900

    fix error

--- src/map.hpp
+++ src/map.hpp
@@ -16,17 +16,17 @@ protected:
     //
     // Member types
     //
-    typedef Key                       key_type;
-    typedef T                         mapped_type;
-    typedef std::pair< const Key, T > value_type;
-    typedef std::size_t               size_type;
-    typedef std::ptrdiff_t            difference_type;
-    typedef Compare                   key_compare;
-    typedef Allocator                 allocator_type;
-    typedef value_type&               reference;
-    typedef const value_type&         const_reference;
-    typedef Allocator::pointer        pointer;
-    typedef Allocator::const_pointer  const_pointer;
+    typedef Key                               key_type;
+    typedef T                                 mapped_type;
+    typedef std::pair< const Key, T >         value_type;
+    typedef std::size_t                       size_type;
+    typedef std::ptrdiff_t                    difference_type;
+    typedef Compare                           key_compare;
+    typedef Allocator                         allocator_type;
+    typedef value_type&                       reference;
+    typedef const value_type&                 const_reference;
+    typedef typename Allocator::pointer       pointer;
+    typedef typename Allocator::const_pointer const_pointer;
     // iterator
     // const_iterator
     // typedef std::reverse_iterator< iterator >       reverse_iterator;
