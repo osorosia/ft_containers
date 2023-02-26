10ce64df76265b4bcb7562f81767c606c8433db1
osorosia <64416398+osorosia@users.noreply.github.com>
2023-02-27 02:26:30 +0900

    avl

--- sample/avl/print.cpp
+++ sample/avl/print.cpp
@@ -2,8 +2,41 @@
 #include <iostream>
 using namespace std;
 
-void print_node(AVLTree& tree, Node* node, string name) {
-    // node check
+#define PRINT_HEIGHT true
+#define CHECK_HEIGHT false
+
+void print_node_null(string prev_name, string name) {
+    cout << prev_name << "-->" << name << "((.))" << endl;
+}
+
+void print_node(Node* node, string prev_name, string name) {
+    if (node == NULL)
+        return;
+    cout << prev_name << "-->" << name << "((" << node->val_;
+    if (PRINT_HEIGHT)
+        cout << ", " << node->height_;
+    cout << "))" << endl;
+}
+
+void print_node_init(Node* node, string name) {
+    cout << name << "((";
+
+    if (node) {
+        cout << node->val_;
+        if (PRINT_HEIGHT)
+            cout << ", " << node->height_;
+    } else {
+        cout << ".";
+    }
+
+    cout << "))" << endl;
+}
+
+void print_tree(AVLTree& tree, Node* node, string name) {
+    if (node == NULL)
+        return;
+
+    // check node
     assert(node->left_ == NULL || node == node->left_->parent_);
     assert(node->right_ == NULL || node == node->right_->parent_);
     if (node != tree.root_) {
@@ -13,25 +46,33 @@ void print_node(AVLTree& tree, Node* node, string name) {
         assert(node->parent_ == NULL);
     }
 
+    // check height
+    if (CHECK_HEIGHT) {
+        if (node->parent_) {
+            assert(node->height_ == node->parent_->height_ - 1);
+        }
+        if (node->left_) {
+            assert(node->height_ == node->left_->height_ + 1);
+        }
+        if (node->right_) {
+            assert(node->height_ == node->right_->height_ + 1);
+        }
+        if (node->left_ == NULL && node->right_ == NULL) {
+            assert(node->height_ == 1);
+        }
+    }
+
     // left
     if (!node->left_ && node->right_)
-        cout << name << "-->" << name + "A"
-             << "((.))" << endl;
-    if (node->left_) {
-        cout << name << "-->" << name + "A"
-             << "((" << node->left_->val_ << "))" << endl;
-        print_node(tree, node->left_, name + "A");
-    }
+        print_node_null(name, name + "A");
+    print_node(node->left_, name, name + "A");
+    print_tree(tree, node->left_, name + "A");
 
     // right
     if (node->left_ && !node->right_)
-        cout << name << "-->" << name + "B"
-             << "((.))" << endl;
-    if (node->right_) {
-        cout << name << "-->" << name + "B"
-             << "((" << node->right_->val_ << "))" << endl;
-        print_node(tree, node->right_, name + "B");
-    }
+        print_node_null(name, name + "B");
+    print_node(node->right_, name, name + "B");
+    print_tree(tree, node->right_, name + "B");
 }
 
 void print(AVLTree& tree) {
@@ -39,8 +80,9 @@ void print(AVLTree& tree) {
     cout << "graph TB" << endl;
 
     if (tree.root_) {
-        cout << "O((" << tree.root_->val_ << "))" << endl;
-        print_node(tree, tree.root_, "O");
+        string name = "O";
+        print_node_init(tree.root_, name);
+        print_tree(tree, tree.root_, name);
     }
 
     cout << "```" << endl;
