608d6227efed98b5e3db6eec4deb59f235038adb
osorosia <64416398+osorosia@users.noreply.github.com>
2023-03-06 07:28:38 +0900

    vector

--- src/vector_test.cpp
+++ src/vector_test.cpp
@@ -3,7 +3,7 @@
 typedef ft::vector< int >  ft_vector;
 typedef std::vector< int > std_vector;
 
-typedef std::allocator< int > allocater;
+typedef std::allocator< int > allocater_type;
 
 void vector_test() {
     TEST_NAME("vector");
@@ -16,6 +16,11 @@ void vector_test() {
     // assign
     // --------------------------------------------------------
     // get_allocator
+    {
+        allocater_type alloc = allocater_type();
+        ft_vector      ft_vec(alloc);
+        assert(ft_vec.get_allocator() == alloc);
+    }
     // --------------------------------------------------------
     // at, operator[], front, back
     {
@@ -59,8 +64,8 @@ void vector_test() {
             std_vec.push_back(i);
         }
 
-        const int *ft_arr = ft_vec.data();
-        const int *std_arr = std_vec.data();
+        const int* ft_arr  = ft_vec.data();
+        const int* std_arr = std_vec.data();
 
         for (int i = 0; i < 10; i++) {
             assert(ft_arr[i] == std_arr[i]);
@@ -69,6 +74,32 @@ void vector_test() {
 
     // --------------------------------------------------------
     // begin, end, rbegin, rend
+    {
+        ft_vector  ft_vec;
+        std_vector std_vec;
+
+        for (int i = 0; i < 10; i++) {
+            ft_vec.push_back(i);
+            std_vec.push_back(i);
+
+            // begin, rbegin
+            assert(*(ft_vec.begin()) == *(std_vec.begin()));
+            assert(*(ft_vec.rbegin()) == *(std_vec.rbegin()));
+
+            // end, rend
+            ft_vector::iterator          ft_end_it   = ft_vec.end();
+            ft_vector::reverse_iterator  ft_rend_it  = ft_vec.rend();
+            std_vector::iterator         std_end_it  = std_vec.end();
+            std_vector::reverse_iterator std_rend_it = std_vec.rend();
+            ft_end_it--;
+            ft_rend_it--;
+            std_end_it--;
+            std_rend_it--;
+
+            assert(*ft_end_it == *std_end_it);
+            assert(*ft_rend_it == *std_rend_it);
+        }
+    }
     // --------------------------------------------------------
     // empty, size
     // --------------------------------------------------------
