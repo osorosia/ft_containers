75d05280a41d0aeb11badb839a32b40b4c30c330
osorosia <64416398+osorosia@users.noreply.github.com>
2023-03-05 21:32:34 +0900

    map

--- src/map.hpp
+++ src/map.hpp
@@ -151,12 +151,37 @@ public:
 //
 // Non-member functions
 //
-// operator==
-// operator!=
-// operator<
-// operator<=
-// operator>
-// operator>=
+template < class Key, class T, class Compare, class Alloc >
+bool operator==(const ft::map< Key, T, Compare, Alloc >& lhs,
+                const ft::map< Key, T, Compare, Alloc >& rhs) {
+    return lhs.size() == rhs.size() && ft::equal(lhs.begin(), lhs.end(), rhs.begin());
+}
+template < class Key, class T, class Compare, class Alloc >
+bool operator!=(const ft::map< Key, T, Compare, Alloc >& lhs,
+                const ft::map< Key, T, Compare, Alloc >& rhs) {
+    return !(lhs == rhs);
+}
+template < class Key, class T, class Compare, class Alloc >
+bool operator<(const ft::map< Key, T, Compare, Alloc >& lhs,
+               const ft::map< Key, T, Compare, Alloc >& rhs) {
+    return ft::lexicographical_compare(lhs.begin(), lhs.end(), rhs.begin(), rhs.end());
+}
+template < class Key, class T, class Compare, class Alloc >
+bool operator<=(const ft::map< Key, T, Compare, Alloc >& lhs,
+                const ft::map< Key, T, Compare, Alloc >& rhs) {
+    return !(rhs < lhs);
+}
+template < class Key, class T, class Compare, class Alloc >
+bool operator>(const ft::map< Key, T, Compare, Alloc >& lhs,
+               const ft::map< Key, T, Compare, Alloc >& rhs) {
+    return rhs < lhs;
+}
+template < class Key, class T, class Compare, class Alloc >
+bool operator>=(const ft::map< Key, T, Compare, Alloc >& lhs,
+                const ft::map< Key, T, Compare, Alloc >& rhs) {
+    return !(lhs < rhs);
+}
+// TODO:
 // swap
 
 } // namespace ft
